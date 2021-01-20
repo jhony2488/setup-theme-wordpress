04dcbb7bc2f3c9003aeaa5821a9501be49ed911e
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-01-20 19:09:51 -0300

    config respimage librarie

--- src/assets/partials/head.php
+++ src/assets/partials/head.php
@@ -11,6 +11,18 @@
             <script src="./assets/scripts/js/jQuery.js"></script>
             <script async defer src="./assets/scripts/js/allPolify.js"></script>
         <![endif]-->
+    <script>
+        function loadJS(u) {
+            const r = document.getElementsByTagName("script")[0],
+                s = document.createElement("script");
+            s.src = u;
+            r.parentNode.insertBefore(s, r);
+        }
+        if (!window.HTMLPictureElement) {
+            document.createElement('picture');
+            loadJS("./assets/scripts/js/respimage.min.js");
+        }
+    </script>
     <link rel="stylesheet" type="text/css" href="./assets/css/main.css" media="print" onload="this.media='all'" />
     <link href="./assets/fontawesome/css/all.css" rel="stylesheet" type="text/css" media="print" onload="this.media='all'" />
 </head>
